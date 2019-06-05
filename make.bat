ed9372b3578c297eb12447c87233e712c8495586
Pavlo Golub <pavlo.golub@gmail.com>
2019-06-05 19:12:20 +0300

    final version. produces correct "output" folder. May be zipped, or packaged by installer

--- make.bat
+++ make.bat
@@ -6,26 +6,29 @@ SET PATRONI_REF=https://github.com/zalando/patroni/archive/v1.5.6.zip
 
 ECHO --- Start bootstrapping ---
 
-RMDIR /Q /S %MD% patroni
+RMDIR /Q /S %MD% patroni > nul
 MKDIR %MD%
-copy src\*.* %MD%\
+COPY src\*.* %MD%\
 
 @ECHO on
 
-
-rem @ECHO --- Download ETCD ---
-rem powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; ((new-object net.webclient).DownloadFile('%ETCD_REF%', '%TEMP%\etcd.zip'))"
-rem powershell -Command "$shell = New-Object -ComObject Shell.Application; 	$zip_src = $shell.NameSpace('%TEMP%\etcd.zip'); $zip_dest = $shell.NameSpace((Resolve-Path '%CD%').Path); $zip_dest.CopyHere($zip_src.Items(), 1044)"
-rem MOVE etcd-* output\etcd
+@ECHO --- Download ETCD ---
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; ((new-object net.webclient).DownloadFile('%ETCD_REF%', '%TEMP%\etcd.zip'))"
+powershell -Command "$shell = New-Object -ComObject Shell.Application; 	$zip_src = $shell.NameSpace('%TEMP%\etcd.zip'); $zip_dest = $shell.NameSpace((Resolve-Path '%CD%').Path); $zip_dest.CopyHere($zip_src.Items(), 1044)"
+MOVE etcd-* output\etcd
 
 @ECHO --- Download PATRONI ---
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; ((new-object net.webclient).DownloadFile('%PATRONI_REF%', '%TEMP%\patroni.zip'))"
 powershell -Command "$shell = New-Object -ComObject Shell.Application; 	$zip_src = $shell.NameSpace('%TEMP%\patroni.zip'); $zip_dest = $shell.NameSpace((Resolve-Path '%CD%').Path); $zip_dest.CopyHere($zip_src.Items(), 1044)"
 MOVE patroni-* patroni
 
-rem CD patroni
-rem virtualenv.exe venv || EXIT /B 1
-rem CALL venv\Scripts\activate || EXIT /B 1
-rem pip install -r requirements.txt || EXIT /B 1
-rem pip install psycopg2-binary || EXIT /B 1
-rem CALL venv\Scripts\deactivate || EXIT /B 1
+CD patroni
+virtualenv.exe venv
+CALL venv\Scripts\activate || EXIT /B 1
+pip install -r requirements.txt || EXIT /B 1
+pip install psycopg2-binary || EXIT /B 1
+CALL venv\Scripts\deactivate || EXIT /B 1
+
+MOVE venv ..\output\venv
+CD ..
+MOVE patroni output\patroni
