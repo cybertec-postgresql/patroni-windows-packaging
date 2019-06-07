2e3e543b3fb17387ee652c9c9362f61b183973d7
Pavlo Golub <pavlo.golub@gmail.com>
2019-06-07 15:14:12 +0300

    Hard coded strings removed

--- make.bat
+++ make.bat
@@ -1,5 +1,6 @@
 @ECHO off
 SET MD=output
+SET PACKAGE_NAME=patroni-win-x64
 SET ETCD_REF=https://github.com/etcd-io/etcd/releases/download/v3.3.13/etcd-v3.3.13-windows-amd64.zip
 SET PATRONI_REF=https://github.com/zalando/patroni/archive/v1.5.6.zip
 
@@ -15,7 +16,7 @@ COPY src\*.* %MD%\
 @ECHO --- Download ETCD ---
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; ((new-object net.webclient).DownloadFile('%ETCD_REF%', '%TEMP%\etcd.zip'))"
 powershell -Command "$shell = New-Object -ComObject Shell.Application; 	$zip_src = $shell.NameSpace('%TEMP%\etcd.zip'); $zip_dest = $shell.NameSpace((Resolve-Path '%CD%').Path); $zip_dest.CopyHere($zip_src.Items(), 1044)"
-MOVE etcd-* output\etcd
+MOVE etcd-* %MD%\etcd
 
 @ECHO --- Download PATRONI ---
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; ((new-object net.webclient).DownloadFile('%PATRONI_REF%', '%TEMP%\patroni.zip'))"
@@ -29,6 +30,7 @@ pip install -r requirements.txt || EXIT /B 1
 pip install psycopg2-binary || EXIT /B 1
 CALL venv\Scripts\deactivate || EXIT /B 1
 
-MOVE venv ..\output\venv
+MOVE venv ..\%MD%\venv
 CD ..
-MOVE patroni output\patroni
+MOVE patroni %MD%\patroni
+MOVE %MD% %PACKAGE_NAME%
