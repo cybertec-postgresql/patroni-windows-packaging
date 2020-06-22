6a66ac1ec2e9207cecc80e02014c1bfedd9d4a42
Pavlo Golub <pavlo.golub@gmail.com>
2020-06-22 19:49:47 +0200

    [-] fix pip calls after the installation of the python

--- src/install.bat
+++ src/install.bat
@@ -1,12 +1,17 @@
-@ECHO on
+@ECHO off
+
+SET PYTHON=%LOCALAPPDATA%\Programs\Python\Python38\python.exe
+SET PIP=%LOCALAPPDATA%\Programs\Python\Python38\Scripts\pip3.exe
 
 @ECHO --- Installing Python runtime ---
-python-install.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+python-install.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0 Include_launcher=0
 @ECHO --- Python runtime installed ---
 
 @ECHO --- Installing Patroni packages ---
-python -m pip install --upgrade pip
+%PYTHON% -m pip install --upgrade pip
 CD patroni
-pip install --no-index --find-links .patroni-packages -r requirements.txt
-pip install --no-index --find-links .patroni-packages psycopg2-binary
+%PIP% install --no-index --find-links .patroni-packages -r requirements.txt
+%PIP% install --no-index --find-links .patroni-packages psycopg2-binary
 @ECHO --- Patroni packages installed ---
+
+@PAUSE
