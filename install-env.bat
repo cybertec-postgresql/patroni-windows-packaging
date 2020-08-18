e14088b59926ba0da511b64989a0803815ed56d4
Pavlo Golub <pavlo.golub@gmail.com>
2020-08-18 17:49:10 +0200

    [*] make "install-env.bat" install Python system-wide

--- install-env.bat
+++ install-env.bat
@@ -5,5 +5,5 @@ SET PYTHON=%LOCALAPPDATA%\Programs\Python\Python38\python.exe
 SET PIP=%LOCALAPPDATA%\Programs\Python\Python38\Scripts\pip3.exe
 
 curl %PYTHON_REF% --output python-install.exe
-python-install.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0 Include_launcher=0
+python-install.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 Include_launcher=0
 %PYTHON% -m pip install --upgrade pip
