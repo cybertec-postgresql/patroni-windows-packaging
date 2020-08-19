f0291ebb715a9820657792e928c5220b2d7132fc
Pavlo Golub <pavlo.golub@gmail.com>
2020-08-19 14:57:36 +0200

    [+] add vip-manager [*] move services to appropriate folders

--- make.bat
+++ make.bat
@@ -4,13 +4,14 @@ SET ETCD_REF=https://github.com/etcd-io/etcd/releases/download/v3.3.22/etcd-v3.3
 SET PATRONI_REF=https://github.com/zalando/patroni/archive/v1.6.5.zip
 SET MICRO_REF=https://github.com/zyedidia/micro/releases/download/v2.0.6/micro-2.0.6-win64.zip
 SET WINSW_REF=https://github.com/winsw/winsw/releases/download/v2.9.0/WinSW.NET461.exe
+SET VIP_REF=https://github.com/cybertec-postgresql/vip-manager/releases/download/v1.0-beta/vip-manager.zip
 
 @ECHO --- Start bootstrapping ---
 
 RMDIR /Q /S %MD% patroni > nul 2>&1
 DEL %MD%.zip > nul 2>&1
 MKDIR %MD%
-COPY src\*.* %MD%\
+COPY src\*.bat %MD%\
 
 @ECHO --- Update Python and PIP installation ---
 CALL install-env.bat
@@ -29,28 +30,35 @@ powershell -Command "Expand-Archive '%TEMP%\micro.zip' '%CD%'"
 MOVE micro-* %MD%\micro
 @ECHO --- MICRO downloaded ---
 
+@ECHO --- Download VIP-MANAGER ---
+curl %VIP_REF% --location --output %TEMP%\vip.zip
+powershell -Command "Expand-Archive '%TEMP%\vip.zip' '%CD%'"
+MOVE vip-manager* %MD%\vip-manager
+@ECHO --- VIP-MANAGER downloaded ---
+
 @ECHO --- Download WINSW ---
-MKDIR %MD%\service
-curl %WINSW_REF% --location --output %MD%\service\patroni_service.exe
-COPY %MD%\service\patroni_service.exe %MD%\service\etcd_service.exe /B
-COPY %MD%\service\patroni_service.exe %MD%\service\vip_service.exe /B
-MOVE %MD%\*.xml %MD%\service\
+curl %WINSW_REF% --location --output %MD%\patroni\patroni_service.exe
+COPY src\patroni_service.xml %MD%\etcd\
+COPY %MD%\patroni\patroni_service.exe %MD%\etcd\etcd_service.exe /B
+COPY src\etcd_service.xml %MD%\etcd\
+COPY %MD%\patroni\patroni_service.exe %MD%\vip-manager\vip_service.exe /B
+COPY src\vip_service.xml %MD%\vip-manager\
 @ECHO --- WINSW downloaded ---
 
 @ECHO --- Download PATRONI ---
 curl %PATRONI_REF% --location --output %TEMP%\patroni.zip
 powershell -Command "Expand-Archive '%TEMP%\patroni.zip' '%CD%'"
-MOVE patroni-* patroni
+MOVE patroni-* %MD%\patroni
+DEL %MD%\patroni\postgres?.yml
+MOVE %MD%\patroni.yml %MD%\patroni\
 @ECHO --- PATRONI downloaded ---
 
 @ECHO --- Download PATRONI packages ---
-CD patroni
+CD %MD%\patroni
 %PIP% download -r requirements.txt -d .patroni-packages
 %PIP% download psycopg2-binary -d .patroni-packages
+CD ..\..
 @ECHO --- PATRONI packages downloaded ---
-
-CD ..
-MOVE patroni %MD%\patroni
 
 @ECHO --- Prepare archive ---
 powershell -Command "Compress-Archive '%MD%' '%MD%.zip'"
