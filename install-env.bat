3070d083db90e6e498cd5e2058ce0d3838cbf295
Pavlo Golub <pavlo.golub@gmail.com>
2020-08-19 14:56:04 +0200

    [-] python is system-wide now

--- install-env.bat
+++ install-env.bat
@@ -1,8 +1,8 @@
 @ECHO off
 
 SET PYTHON_REF=https://www.python.org/ftp/python/3.8.3/python-3.8.3-amd64.exe
-SET PYTHON=%LOCALAPPDATA%\Programs\Python\Python38\python.exe
-SET PIP=%LOCALAPPDATA%\Programs\Python\Python38\Scripts\pip3.exe
+SET PYTHON=python.exe
+SET PIP=pip3.exe
 
 curl %PYTHON_REF% --output python-install.exe
 python-install.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 Include_launcher=0
