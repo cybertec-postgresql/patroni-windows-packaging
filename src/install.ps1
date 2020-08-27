f84fce707e94eacd17d1d1e2e5fcc76219d25212
Pavlo Golub <pavlo.golub@gmail.com>
2020-08-27 12:29:24 +0200

    [+] Add check for existing patroni user, resolves #12

--- src/install.ps1
+++ src/install.ps1
@@ -13,12 +13,21 @@ pip3.exe install --no-index --find-links .patroni-packages psycopg2-binary
 Set-Location '..'
 Write-Host "--- Patroni packages installed ---`n" -ForegroundColor green
 
-Write-Host "--- Adding local user 'pes' for patroni service ---" -ForegroundColor blue
-$Password = -join ((65..90) + (97..122) | Get-Random -Count 8 | % {[char]$_})
-$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
-New-LocalUser "pes" -Password $SecurePassword -Description "Patroni service account"
-$ConfFile = 'patroni\patroni_service.xml'
-(Get-Content $ConfFile) -replace '12345', $Password | Out-File -encoding ASCII $ConfFile
-Write-Host "--- Patroni user added ---`n" -ForegroundColor green
+$userName = "pes"
+$out = Get-LocalUser -Name $userName -ErrorAction SilentlyContinue
+if($out -eq $null)
+{
+    Write-Host "--- Adding local user '$userName' for patroni service ---" -ForegroundColor blue
+    $Password = -join ((65..90) + (97..122) | Get-Random -Count 8 | % {[char]$_})
+    $SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+    New-LocalUser $userName -Password $SecurePassword -Description "Patroni service account"
+    $ConfFile = 'patroni\patroni_service.xml'
+    (Get-Content $ConfFile) -replace '12345', $Password | Out-File -encoding ASCII $ConfFile
+    Write-Host "--- Patroni user '$userName' added ---`n" -ForegroundColor green
+}
+else
+{
+    Write-Host "--- Patroni user '$userName' already exists ---`n" -ForegroundColor green
+}
 
 Write-Host "--- Installation sucessfully finished ---" -ForegroundColor green
