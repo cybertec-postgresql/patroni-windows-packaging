ec20da37dbd349aff777ca24e5b5143b5056c449
Pavlo Golub <pavlo.golub@gmail.com>
2022-02-09 14:06:09 +0100

    [+] before producing installer remove old possible leftovers [*] move Python installation right before it needed [-] add timeouts after archive extracting, fixes #26 [+] add fail fast instructions (`EXIT /B`) for all commands [*] deletes files after including to archive

--- make.bat
+++ make.bat
@@ -11,88 +11,111 @@ SET PES_REF=https://github.com/cybertec-postgresql/PES/releases/download/v0.1/pe
 SET SEVENZIP="C:\Program Files\7-Zip\7z.exe"
 
 @ECHO --- Start bootstrapping ---
-
 RMDIR /Q /S %MD% patroni > nul 2>&1
 DEL %MD%.zip > nul 2>&1
+DEL %TEMP%\etcd.zip > nul 2>&1
+DEL %TEMP%\pes.zip > nul 2>&1
+DEL %TEMP%\micro.zip > nul 2>&1
+DEL %TEMP%\vip.zip > nul 2>&1
+DEL %TEMP%\pgsql.zip > nul 2>&1
+DEL %TEMP%\patroni.zip > nul 2>&1
 DEL Patroni-Env-Setup.exe > nul 2>&1
-MKDIR %MD%
-COPY src\*.bat %MD%\
-COPY src\*.ps1 %MD%\
-XCOPY doc %MD%\doc\ /E
+MKDIR %MD% || EXIT /B
+COPY src\*.bat %MD%\ || EXIT /B
+COPY src\*.ps1 %MD%\ || EXIT /B
+XCOPY doc %MD%\doc\ /E || EXIT /B
+@ECHO --- End bootstrapping ---
 
-@ECHO --- Update Python and PIP installation ---
-CALL install-env.bat
-MOVE python-install.exe %MD%\
-@ECHO --- Python and PIP installation updated ---
 
 @ECHO --- Download ETCD ---
-curl %ETCD_REF% --location --output %TEMP%\etcd.zip
+curl %ETCD_REF% --location --output %TEMP%\etcd.zip || EXIT /B
 if exist %SEVENZIP% (
-    %SEVENZIP% x "%TEMP%\etcd.zip" -y -mmt -o"%CD%"
+    %SEVENZIP% x "%TEMP%\etcd.zip" -y -o"%CD%" || EXIT /B
 ) else (
-    powershell -Command "Expand-Archive '%TEMP%\etcd.zip' '%CD%'"
+    powershell -Command "Expand-Archive '%TEMP%\etcd.zip' '%CD%'" || EXIT /B
 )
-MOVE etcd-* %MD%\etcd
-COPY src\etcd.yaml %MD%\etcd\
-DEL %TEMP%\etcd.zip
+REM timeouts are used here to give 7-Zip some time to release output folder and prevent "Access denied" error
+TIMEOUT 5 
+MOVE etcd-* %MD%\etcd || EXIT /B
+COPY src\etcd.yaml %MD%\etcd\ || EXIT /B
+DEL %TEMP%\etcd.zip || EXIT /B
 @ECHO --- ETCD downloaded ---
 
+
+
 @ECHO --- Download PES GUI ---
-curl %PES_REF% --location --output %TEMP%\pes.zip
+curl %PES_REF% --location --output %TEMP%\pes.zip || EXIT /B
 if exist %SEVENZIP% (
-    %SEVENZIP% x "%TEMP%\pes.zip" -y -mmt -o"%MD%"
+    %SEVENZIP% x "%TEMP%\pes.zip" -y -o"%MD%"
 ) else (
     powershell -Command "Expand-Archive '%TEMP%\pes.zip' '%MD%'"
 )
-DEL %TEMP%\pes.zip
+DEL %TEMP%\pes.zip || EXIT /B
 @ECHO --- PES GUI downloaded ---
 
 @ECHO --- Download MICRO ---
-curl %MICRO_REF% --location --output %TEMP%\micro.zip
+curl %MICRO_REF% --location --output %TEMP%\micro.zip || EXIT /B
 if exist %SEVENZIP% (
-    %SEVENZIP% x "%TEMP%\micro.zip" -y -mmt -o"%CD%"
+    %SEVENZIP% x "%TEMP%\micro.zip" -y -o"%CD%"
 ) else (
     powershell -Command "Expand-Archive '%TEMP%\micro.zip' '%CD%'"
 )
-MOVE micro-* %MD%\micro
-DEL %TEMP%\micro.zip
+TIMEOUT 5
+MOVE micro-* %MD%\micro || EXIT /B
+DEL %TEMP%\micro.zip || EXIT /B
 @ECHO --- MICRO downloaded ---
 
 @ECHO --- Download VIP-MANAGER ---
-curl %VIP_REF% --location --output %TEMP%\vip.zip
+curl %VIP_REF% --location --output %TEMP%\vip.zip || EXIT /B
 if exist %SEVENZIP% (
-    %SEVENZIP% x "%TEMP%\vip.zip" -y -mmt -o"%CD%"
+    %SEVENZIP% x "%TEMP%\vip.zip" -y -o"%CD%"
 ) else (
     powershell -Command "Expand-Archive '%TEMP%\vip.zip' '%CD%'"
 )
-MOVE vip-manager* %MD%\vip-manager
-DEL %TEMP%\vip.zip
+TIMEOUT 5
+MOVE vip-manager* %MD%\vip-manager || EXIT /B
+DEL %TEMP%\vip.zip || EXIT /B
 @ECHO --- VIP-MANAGER downloaded ---
 
+
+
 @ECHO --- Download POSTGRESQL ---
-curl %PGSQL_REF% --location --output %TEMP%\pgsql.zip
+curl %PGSQL_REF% --location --output %TEMP%\pgsql.zip || EXIT /B
 if exist %SEVENZIP% (
-    %SEVENZIP% x "%TEMP%\pgsql.zip" -y -mmt -o"%CD%"
+    %SEVENZIP% x "%TEMP%\pgsql.zip" -y -o"%CD%"
 ) else (
     powershell -Command "Expand-Archive '%TEMP%\pgsql.zip' '%CD%'"
 )
-MOVE pgsql* %MD%\pgsql
-RMDIR /Q /S "%MD%\pgsql\pgAdmin 4" "%MD%\pgsql\symbols"
-DEL %TEMP%\pgsql.zip
+TIMEOUT 5
+MOVE pgsql* %MD%\pgsql || EXIT /B
+RMDIR /Q /S "%MD%\pgsql\pgAdmin 4" "%MD%\pgsql\symbols" || EXIT /B
+DEL %TEMP%\pgsql.zip || EXIT /B
 @ECHO --- POSTGRESQL downloaded ---
 
+
+
 @ECHO --- Download PATRONI ---
-curl %PATRONI_REF% --location --output %TEMP%\patroni.zip
+curl %PATRONI_REF% --location --output %TEMP%\patroni.zip || EXIT /B
 if exist %SEVENZIP% (
-    %SEVENZIP% x "%TEMP%\patroni.zip" -y -mmt -o"%CD%"
+    %SEVENZIP% x "%TEMP%\patroni.zip" -y -o"%CD%"
 ) else (
     powershell -Command "Expand-Archive '%TEMP%\patroni.zip' '%CD%'"
 )
-MOVE patroni-* %MD%\patroni
-DEL %MD%\patroni\postgres?.yml
-COPY src\patroni.yaml %MD%\patroni\
-DEL %TEMP%\patroni.zip
+TIMEOUT 5 
+MOVE patroni-* %MD%\patroni || EXIT /B
+DEL %MD%\patroni\postgres?.yml || EXIT /B
+COPY src\patroni.yaml %MD%\patroni\ || EXIT /B
+DEL %TEMP%\patroni.zip || EXIT /B
 @ECHO --- PATRONI downloaded ---
+
+
+
+@ECHO --- Update Python and PIP installation ---
+CALL install-env.bat || EXIT /B
+MOVE python-install.exe %MD%\ || EXIT /B
+@ECHO --- Python and PIP installation updated ---
+
+
 
 @ECHO --- Download PATRONI packages ---
 CD %MD%\patroni
@@ -101,26 +124,34 @@ CD %MD%\patroni
 CD ..\..
 @ECHO --- PATRONI packages downloaded ---
 
+
+
 @ECHO --- Download WINSW ---
-curl %WINSW_REF% --location --output %MD%\patroni\patroni_service.exe
-COPY src\patroni_service.xml %MD%\patroni\
-COPY %MD%\patroni\patroni_service.exe %MD%\etcd\etcd_service.exe /B
-COPY src\etcd_service.xml %MD%\etcd\
-COPY %MD%\patroni\patroni_service.exe %MD%\vip-manager\vip_service.exe /B
-COPY src\vip_service.xml %MD%\vip-manager\
+curl %WINSW_REF% --location --output %MD%\patroni\patroni_service.exe || EXIT /B
+COPY src\patroni_service.xml %MD%\patroni\ || EXIT /B
+COPY %MD%\patroni\patroni_service.exe %MD%\etcd\etcd_service.exe /B || EXIT /B
+COPY src\etcd_service.xml %MD%\etcd\ || EXIT /B
+COPY %MD%\patroni\patroni_service.exe %MD%\vip-manager\vip_service.exe /B || EXIT /B
+COPY src\vip_service.xml %MD%\vip-manager\ || EXIT /B
 @ECHO --- WINSW downloaded ---
+
+
+
+@ECHO --- Creating windows installer ---
+CALL make-installer.bat || EXIT /B
+@ECHO --- Installer generated successfully ---
+
+
 
 @ECHO --- Prepare archive ---
 if exist %SEVENZIP% (
-    %SEVENZIP% a "%MD%.zip" -y -mmt "%MD%"
+    %SEVENZIP% a "%MD%.zip" -y "%MD%" -sdel
 ) else (
     powershell -Command "Compress-Archive '%MD%' '%MD%.zip'"
 )
 @ECHO --- Archive compressed ---
 
-@ECHO --- Creating windows installer ---
-CALL make-installer.bat
-@ECHO --- Installer generated successfully ---
+
 
 @ECHO --- PACKAGING FINISHED ---
 
