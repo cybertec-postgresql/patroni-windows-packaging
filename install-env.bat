c4134fd16c4a85854442e631f7a2d6f7a076f560
Pavlo Golub <pavlo.golub@gmail.com>
2022-05-09 10:33:57 +0200

    [-] fix `install-end.bat` to use just installed python version, fixes #29

--- install-env.bat
+++ install-env.bat
@@ -1,9 +1,15 @@
 @ECHO off
 
-SET PYTHON_REF=https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe
-SET PYTHON=python.exe
-SET PIP=pip3.exe
+SET "PYTHON_REF=https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe"
+SET PYTHON="%ProgramFiles%\Python310\python.exe"
+SET PIP="%ProgramFiles%\Python310\Scripts\pip3.exe"
 
 curl %PYTHON_REF% --output python-install.exe
 python-install.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 Include_launcher=0
 %PYTHON% -m pip install --upgrade pip
+
+echo Python version is:
+%PYTHON% --version
+
+echo PIP version is:
+%PIP% --version
