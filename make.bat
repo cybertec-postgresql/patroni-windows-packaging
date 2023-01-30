aca11133a6e34c2dc0ce8cff6ec3a1cdf8d3310b
Pavlo Golub <pavlo.golub@gmail.com>
2023-01-30 12:23:02 +0100

    [+] bump vip-manager to v2.1.0

--- make.bat
+++ make.bat
@@ -4,7 +4,7 @@ SET ETCD_REF=https://github.com/etcd-io/etcd/releases/download/v3.5.7/etcd-v3.5.
 SET PATRONI_REF=https://github.com/zalando/patroni/archive/refs/tags/v3.0.0.zip
 SET MICRO_REF=https://github.com/zyedidia/micro/releases/download/v2.0.11/micro-2.0.11-win64.zip
 SET WINSW_REF=https://github.com/winsw/winsw/releases/download/v2.12.0/WinSW.NET461.exe
-SET VIP_REF=https://github.com/cybertec-postgresql/vip-manager/releases/download/v2.0.0/vip-manager_2.0.0_Windows_x86_64.zip
+SET VIP_REF=https://github.com/cybertec-postgresql/vip-manager/releases/download/v2.1.0/vip-manager_2.1.0_Windows_x86_64.zip
 SET PGSQL_REF=https://get.enterprisedb.com/postgresql/postgresql-15.1-1-windows-x64-binaries.zip
 SET PES_REF=https://github.com/cybertec-postgresql/PES/releases/download/v0.2/pes.zip
 
