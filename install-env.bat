406c392e088587eb52c63764c79dc19f53f40ec3
Pavlo Golub <pavlo.golub@gmail.com>
2024-05-16 11:33:31 +0200

    [+] bump Python to v3.12.3

--- install-env.bat
+++ install-env.bat
@@ -2,7 +2,7 @@
 
 setlocal enableDelayedExpansion
 
-set "PYTHON_REF=https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe"
+set "PYTHON_REF=https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe"
 @REM one should change python version in github action workflows when changed here
 
 set PYTHON="python.exe"
