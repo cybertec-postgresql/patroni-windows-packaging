8f7336e2736127ef488e34ab16c335b9567a36f6
Pavlo Golub <pavlo.golub@gmail.com>
2024-09-18 07:39:19 +0200

    [!] bump Patroni to v4.0.1 (#84)
    [!] bump Patroni to v4.0.2
    [+] bump micro to v2.0.14
    [+] bump etcd to v3.5.16

--- make.bat
+++ make.bat
@@ -1,9 +1,9 @@
 @ECHO off
 SET MD=PES
 SET VCREDIST_REF=https://aka.ms/vs/17/release/vc_redist.x64.exe
-SET ETCD_REF=https://github.com/etcd-io/etcd/releases/download/v3.5.15/etcd-v3.5.15-windows-amd64.zip
-SET PATRONI_REF=https://github.com/patroni/patroni/archive/refs/tags/v3.3.2.zip
-SET MICRO_REF=https://github.com/zyedidia/micro/releases/download/v2.0.13/micro-2.0.13-win64.zip
+SET ETCD_REF=https://github.com/etcd-io/etcd/releases/download/v3.5.16/etcd-v3.5.16-windows-amd64.zip
+SET PATRONI_REF=https://github.com/patroni/patroni/archive/refs/tags/v4.0.2.zip
+SET MICRO_REF=https://github.com/zyedidia/micro/releases/download/v2.0.14/micro-2.0.14-win64.zip
 SET WINSW_REF=https://github.com/winsw/winsw/releases/download/v2.12.0/WinSW.NET461.exe
 SET VIP_REF=https://github.com/cybertec-postgresql/vip-manager/releases/download/v2.6.0/vip-manager_2.6.0_Windows_x86_64.zip
 SET PGSQL_REF=https://get.enterprisedb.com/postgresql/postgresql-16.4-1-windows-x64-binaries.zip
