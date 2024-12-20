5d88b5322e71ec2dc6ba0ff70726bfe854f29659
Pavlo Golub <pavlo.golub@gmail.com>
2024-12-20 14:28:53 +0100

    [+] bump Patroni to v4.0.4

--- env.ps1
+++ env.ps1
@@ -1,7 +1,7 @@
 $MD = "PES"
 $VCREDIST_REF = "https://aka.ms/vs/17/release/vc_redist.x64.exe"
 $ETCD_REF = "https://github.com/etcd-io/etcd/releases/download/v3.5.17/etcd-v3.5.17-windows-amd64.zip"
-$PATRONI_REF = "https://github.com/patroni/patroni/archive/refs/tags/v4.0.2.zip"
+$PATRONI_REF = "https://github.com/patroni/patroni/archive/refs/tags/v4.0.4.zip"
 $MICRO_REF = "https://github.com/zyedidia/micro/releases/download/v2.0.14/micro-2.0.14-win64.zip"
 $WINSW_REF = "https://github.com/winsw/winsw/releases/download/v2.12.0/WinSW.NET461.exe"
 $VIP_REF = "https://github.com/cybertec-postgresql/vip-manager/releases/download/v2.6.0/vip-manager_2.6.0_Windows_x86_64.zip"
