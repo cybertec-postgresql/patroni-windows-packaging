9eab20e86280fdd1b93e42bc756cfc617e33f05a
Pavlo Golub <pavlo.golub@gmail.com>
2024-12-20 14:29:33 +0100

    [+] bump PostgreSQL to v17.2

--- env.ps1
+++ env.ps1
@@ -5,7 +5,7 @@ $PATRONI_REF = "https://github.com/patroni/patroni/archive/refs/tags/v4.0.4.zip"
 $MICRO_REF = "https://github.com/zyedidia/micro/releases/download/v2.0.14/micro-2.0.14-win64.zip"
 $WINSW_REF = "https://github.com/winsw/winsw/releases/download/v2.12.0/WinSW.NET461.exe"
 $VIP_REF = "https://github.com/cybertec-postgresql/vip-manager/releases/download/v3.0.0/vip-manager_3.0.0_Windows_x86_64.zip"
-$PGSQL_REF = "https://get.enterprisedb.com/postgresql/postgresql-16.4-1-windows-x64-binaries.zip"
+$PGSQL_REF = "https://get.enterprisedb.com/postgresql/postgresql-17.2-1-windows-x64-binaries.zip"
 $PYTHON_REF = "https://www.python.org/ftp/python/3.13.1/python-3.13.1-amd64.exe"
 # one should change python version in github action workflows when changed here
 
