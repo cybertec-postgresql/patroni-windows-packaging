ae7e98572d3408ec3bc27166ad384dc7be26224f
András Váczi <andras.vaczi@cybertec.at>
2025-01-08 10:34:25 +0100

    Prepare release with multisite and Postgres 15

--- env.ps1
+++ env.ps1
@@ -1,11 +1,11 @@
 $MD = "PES"
 $VCREDIST_REF = "https://aka.ms/vs/17/release/vc_redist.x64.exe"
 $ETCD_REF = "https://github.com/etcd-io/etcd/releases/download/v3.5.17/etcd-v3.5.17-windows-amd64.zip"
-$PATRONI_REF = "https://github.com/patroni/patroni/archive/refs/tags/v4.0.4.zip"
+$PATRONI_REF = "https://github.com/cybertec-postgresql/patroni/archive/refs/tags/multisite-4.0.4.1.zip"
 $MICRO_REF = "https://github.com/zyedidia/micro/releases/download/v2.0.14/micro-2.0.14-win64.zip"
 $WINSW_REF = "https://github.com/winsw/winsw/releases/download/v2.12.0/WinSW.NET461.exe"
 $VIP_REF = "https://github.com/cybertec-postgresql/vip-manager/releases/download/v3.0.0/vip-manager_3.0.0_Windows_x86_64.zip"
-$PGSQL_REF = "https://get.enterprisedb.com/postgresql/postgresql-17.2-1-windows-x64-binaries.zip"
+$PGSQL_REF = "https://get.enterprisedb.com/postgresql/postgresql-15.10-1-windows-x64-binaries.zip"
 $PYTHON_REF = "https://www.python.org/ftp/python/3.13.1/python-3.13.1-amd64.exe"
 # one should change python version in github action workflows when changed here
 
