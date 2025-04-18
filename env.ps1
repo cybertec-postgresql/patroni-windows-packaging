8862ef4fc7ca632813a442b9a64cb4825c50a6fd
avandras <andras.vaczi@cybertec.at>
2025-04-18 09:18:23 +0200

    Set Patroni to multisite working branch

--- env.ps1
+++ env.ps1
@@ -1,7 +1,7 @@
 $MD = "PES"
 $VCREDIST_REF = "https://aka.ms/vs/17/release/vc_redist.x64.exe"
 $ETCD_REF = "https://github.com/etcd-io/etcd/releases/download/v3.5.18/etcd-v3.5.18-windows-amd64.zip"
-$PATRONI_REF = "https://github.com/patroni/patroni/archive/refs/tags/v4.0.5.zip"
+$PATRONI_REF = "https://github.com/cybertec-postgresql/patroni/archive/refs/heads/ctl-switchover-only.zip"
 $MICRO_REF = "https://github.com/zyedidia/micro/releases/download/v2.0.14/micro-2.0.14-win64.zip"
 $WINSW_REF = "https://github.com/winsw/winsw/releases/download/v2.12.0/WinSW.NET461.exe"
 $VIP_REF = "https://github.com/cybertec-postgresql/vip-manager/releases/download/v3.0.0/vip-manager_3.0.0_Windows_x86_64.zip"
