82fd0fadb1d865d85cbb93b6b36dd5970b612f0d
Pavlo Golub <pavlo.golub@gmail.com>
2026-05-05 16:46:46 +0200

    v050526 release
    [+] bump Postgres to v18.3
    [+] bump Etcd to v3.5.30
    [+] bump Patroni to v4.1.2
    [+] bump vip-manager to v4.2.0
    [+] bump Python to v3.14.4

--- globals.ps1
+++ globals.ps1
@@ -1,12 +1,12 @@
 $MD = "PES"
 $VCREDIST_REF = "https://aka.ms/vs/17/release/vc_redist.x64.exe"
-$ETCD_REF = "https://github.com/etcd-io/etcd/releases/download/v3.5.21/etcd-v3.5.21-windows-amd64.zip"
-$PATRONI_REF = "https://github.com/patroni/patroni/archive/refs/tags/v4.0.6.zip"
-$MICRO_REF = "https://github.com/zyedidia/micro/releases/download/v2.0.14/micro-2.0.14-win64.zip"
+$ETCD_REF = "https://github.com/etcd-io/etcd/releases/download/v3.5.30/etcd-v3.5.30-windows-amd64.zip"
+$PATRONI_REF = "https://github.com/patroni/patroni/archive/refs/tags/v4.1.2.zip"
+$MICRO_REF = "https://github.com/zyedidia/micro/releases/download/v2.0.15/micro-2.0.15-win64.zip"
 $WINSW_REF = "https://github.com/winsw/winsw/releases/download/v2.12.0/WinSW.NET461.exe"
-$VIP_REF = "https://github.com/cybertec-postgresql/vip-manager/releases/download/v4.0.0/vip-manager_4.0.0_Windows_x86_64.zip"
-$PGSQL_REF = "https://get.enterprisedb.com/postgresql/postgresql-17.5-1-windows-x64-binaries.zip"
-$PYTHON_REF = "https://www.python.org/ftp/python/3.13.5/python-3.13.5-amd64.exe"
+$VIP_REF = "https://github.com/cybertec-postgresql/vip-manager/releases/download/v4.2.0/vip-manager_4.2.0_Windows_x86_64.zip"
+$PGSQL_REF = "https://get.enterprisedb.com/postgresql/postgresql-18.3-1-windows-x64-binaries.zip"
+$PYTHON_REF = "https://www.python.org/ftp/python/3.14.4/python-3.14.4-amd64.exe"
 # one should change python version in github action workflows when changed here
 
 $SEVENZIP = "C:\Program Files\7-Zip\7z.exe"
